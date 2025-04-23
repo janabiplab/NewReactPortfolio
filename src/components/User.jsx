db07195539014dc2ed9d6d47f2e524972812c773
janabiplab <141055149+janabiplab@users.noreply.github.com>
2025-04-23 16:42:19 +0530

    update file

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from "styled-components"
-import Engineer from "../assets/Image/myImage.JPG"
+import Engineer from "../assets/Image/myImage.jpg"
 
 function User() {
   return (
